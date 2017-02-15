00df7827e604d72df44d1a32df6115a7cf70ab4b
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-02-15 18:37:04 +0100

    changes in read_mat

--- I-O/mat.py
+++ I-O/mat.py
@@ -1,53 +1,15 @@
 #       HAKUNA MATATA
 
-import pandas as pd
 from scipy.io import loadmat, savemat
 
 
-def read_mat(filename):
-    """ Read a .mat file and store all possible elements in pandas DataFrame 
-    Parameters
-    ----------
-    filename: str
-        Path tho the filename
+def read_mat(filename, points_name="points", mesh_name="mesh"):
+    """ Read a .mat file and return points and mesh(if exists)
 
-    Returns
-    -------
-    data: dict
-        If possible, elements as pandas DataFrames else input format
     """
-    
     mat = loadmat(filename)
-
-    if "points" in mat:
-        columns = None
-
-        if "points_names" in mat:
-            columns= [mat["points_names"][i].strip() for i in range(len(mat["points_names"]))]
-            del mat["points_names"]
-        
-        mat["points"] = pd.DataFrame(mat["points"], columns=columns)
-
-        if "points_dtypes" in mat:
-            for i in range(len(mat["points_dtypes"])):
-                mat["points"][columns[i]] = mat["points"][columns[i]].astype(mat["points_dtypes"][i].strip())
-            
-            del mat["points_dtypes"]
-            
     
-    if "mesh" in mat:
-        columns = None
-
-        if "mesh_names" in mat:
-            columns= [mat["mesh_names"][i].strip() for i in range(len(mat["mesh_names"]))]
-            del mat["mesh_names"]
-        
-        mat["mesh"] = pd.DataFrame(mat["mesh"], columns=columns)
-
-        if "mesh_dtypes" in mat:
-            for i in range(len(mat["mesh_dtypes"])):
-                mat["mesh"][columns[i]] = mat["mesh"][columns[i]].astype(mat["mesh_dtypes"][i].strip())
-            
-            del mat["mesh_dtypes"]
-            
-    return mat
+    if mesh_name is not None:
+        return mat[points_name], mat[mesh_name]
+    else: 
+        return mat[points_name]
